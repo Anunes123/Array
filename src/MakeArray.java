683d933982a188c4551754d02a45c980aecdfc36
anunes123 <andrew_nunes26@milton.edu>
2025-02-19 11:25:27 -0500

    made array

---
+++ src/MakeArray.java
@@ -0,0 +1,26 @@
+public class MakeArray {
+
+
+    int[] nums= new int [3];
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+MakeArray make = new MakeArray();
+
+    }
+
+    public MakeArray (){
+        System.out.println("welcome to array world");
+nums[1]=5;
+nums[0]=10;
+nums[2]= nums[1]*3;
+printarray();
+    }
+    public void printarray (){
+        System.out.println(nums[0]);
+
+
+    }
+
+
+}
