0ab5d4c9c48d1248d5cff1b4cbbf4e1e2a5e6063
anunes123 <andrew_nunes26@milton.edu>
2025-02-20 17:39:30 -0500

    made array

--- src/MakeArray.java
+++ src/MakeArray.java
@@ -1,7 +1,7 @@
 public class MakeArray {
 
 
-    int[] nums= new int [3];
+    int[] nums= new int [10];
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
@@ -13,11 +13,18 @@ MakeArray make = new MakeArray();
         System.out.println("welcome to array world");
 nums[1]=5;
 nums[0]=10;
-nums[2]= nums[1]*3;
+nums[2]= nums[1]*2;
+nums[3]= nums[1]/5;
+nums[4]= nums[5]*2;
+nums[5]= 3;
+nums[6]=10;
+nums[7]= nums[6]-4;
+nums[8]= nums[6]+5;
+nums[9]= nums[0]+8;
 printarray();
     }
     public void printarray (){
-        System.out.println(nums[0]);
+        System.out.println(nums[0]+nums[1]+nums[2]+nums[3]+nums[4]+nums[5]+nums[6]+nums[7]+nums[8]+nums[9]);
 
 
     }
