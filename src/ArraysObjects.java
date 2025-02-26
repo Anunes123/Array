c0d7024e8b978b4703de056cdb1f7c4d7d5d01e6
anunes123 <andrew_nunes26@milton.edu>
2025-02-26 14:27:59 -0500

    made array Added sushi class and Array objects Did homework

--- src/ArraysObjects.java
+++ src/ArraysObjects.java
@@ -5,9 +5,29 @@ public class ArraysObjects {
     Sushi[] soosh = new Sushi [10];
     public ArraysObjects(){
         System.out.println("making arrays with objects");
-Sushi s = new Sushi(10.99);
-        soosh[0]= s ;
+Sushi a = new Sushi(10.99);
+Sushi b = new Sushi(8.99);
+Sushi c = new Sushi(20.99);
+Sushi d = new Sushi(5.99);
+Sushi e = new Sushi(7.99);
+Sushi f = new Sushi(9.99);
+Sushi g = new Sushi(2.99);
+Sushi h = new Sushi(3.99);
+Sushi i = new Sushi(6.99);
+for (int x = 0; x < soosh.length; x++){
+
+}
+
+        soosh[0]= a ;
         soosh[0].print();
+        soosh[1]= b;
+        soosh[2]= c;
+        soosh[3]= d;
+        soosh[4]= e;
+        soosh[5]= f;
+        soosh[7]= g;
+        soosh[8]= h;
+        soosh[9]= i;
         //fill sushi array with sushi objects 1-9
     }
 }
