2c9c99d8534060aff4731350acfc04ef7c74285c
anunes123 <andrew_nunes26@milton.edu>
2025-03-05 11:25:24 -0500

    made array Added sushi class and Array objects Did homework

--- src/ArraysObjects.java
+++ src/ArraysObjects.java
@@ -2,35 +2,47 @@ public class ArraysObjects {
     public static void main(String[] args) {
         ArraysObjects a = new ArraysObjects();
     }
-    Sushi[] soosh = new Sushi [10];
-    public ArraysObjects(){
+
+    Sushi[] soosh = new Sushi[10];
+
+    public ArraysObjects() {
         System.out.println("making arrays with objects");
-Sushi a = new Sushi(10.99);
-Sushi b = new Sushi(8.99);
-Sushi c = new Sushi(20.99);
-Sushi d = new Sushi(5.99);
-Sushi e = new Sushi(7.99);
-Sushi f = new Sushi(9.99);
-Sushi g = new Sushi(2.99);
-Sushi h = new Sushi(3.99);
-Sushi i = new Sushi(6.99);
-for (int x = 0; x < soosh.length; x++){
+        Sushi a = new Sushi(10.99);
+        Sushi b = new Sushi(8.99);
+        for (int x = 0; x < soosh.length; x++) {
 
-}
+        }
+        for (int x = 2; x < 10; x++) {
+            soosh[x] = new Sushi(Math.random() * 250);
 
-        soosh[0]= a ;
+
+        }
+
+        soosh[0] = a;
         soosh[0].print();
-        soosh[1]= b;
-        soosh[2]= c;
-        soosh[3]= d;
-        soosh[4]= e;
-        soosh[5]= f;
-        soosh[7]= g;
-        soosh[8]= h;
-        soosh[9]= i;
-        //fill sushi array with sushi objects 1-9
+        soosh[1] = b;
+        printArray();
     }
-}
+
+    public void printArray() {
+        for (int x = 0; x < soosh.length; x++) {
+            soosh[x].print();
+
+        }
+    }
+//use for loop
+public void changeSize() {
+    for (int x=0; x< soosh.length; x++){
+        soosh[x].size = (int)(Math.random()*10+10);
+    }
+
+        }
+
+
+
+
+    }
+
 
 
 
